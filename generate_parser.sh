fa36568e80635d42f4df41c28685e9bde6782bc3
Doctor <tixigam@gmail.com>
2026-03-20 07:51:31 +0300

    v0.1.2 delete old file

--- generate_parser.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-curl -O https://www.antlr.org/download/antlr-4.13.2-complete.jar
-$JAVA_HOME/bin/java -cp antlr-4.13.2-complete.jar org.antlr.v4.Tool -package com.oracle.truffle.sl.parser -no-listener -visitor language/src/main/java/com/oracle/truffle/sl/parser/SimpleLanguage.g4
